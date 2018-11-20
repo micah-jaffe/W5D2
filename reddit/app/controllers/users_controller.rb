acc5dec7bd4804cdf927e8e55139945984ded288
micah-jaffe <micah.jaffe95@gmail.com>
2018-11-20 11:36:33 -0800

    Complete auth phase

---
+++ reddit/app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in(@user)
+      # redirect_to 
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    log_out
+    @user.destroy
+    redirect_to new_session_url
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :edit
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def index
+    @users = User.all
+    render :index
+  end
+  
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
